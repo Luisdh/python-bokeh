1b057e43d56659e25faa41392e0b17b16d0b6833
unknown <luisd824@gmail.com>
2018-06-21 18:40:10 -0400

    fix debug_test_data function to properly draw edges to vertexes with hardcode. BFS colors all vertexes same color

--- src/graph.py
+++ src/graph.py
@@ -16,17 +16,32 @@ class Graph:
     self.vertexes = []
 
   def debug_create_test_data(self):
-    debug_vertex_1 = Vertex('t1', x=40, y=40)
-    debug_vertex_2 = Vertex('t2', x=140, y=140)
-    debug_vertex_3 = Vertex('t3', x=300, y=400)
+    debug_vertex_1 = Vertex('t1', x=250, y=50)
+    debug_vertex_2 = Vertex('t2', x=150, y=150)
+    debug_vertex_3 = Vertex('t3', x=150, y=250)
+    debug_vertex_4 = Vertex('t4', x=250, y=350)
+    debug_vertex_5 = Vertex('t5', x=350, y=250)
+    debug_vertex_6 = Vertex('t6', x=350, y=150)
 
     debug_edge_1 = Edge(debug_vertex_2)
     debug_vertex_1.edges.append(debug_edge_1)
 
-    debug_edge_2 = Edge(debug_vertex_2)
-    debug_vertex_3.edges.append(debug_edge_2)
+    debug_edge_2 = Edge(debug_vertex_3)
+    debug_vertex_2.edges.append(debug_edge_2)
 
-    self.vertexes.extend([debug_vertex_1, debug_vertex_2, debug_vertex_3])
+    debug_edge_3 = Edge(debug_vertex_4)
+    debug_vertex_3.edges.append(debug_edge_3)
+
+    debug_edge_4 = Edge(debug_vertex_5)
+    debug_vertex_4.edges.append(debug_edge_4)
+
+    debug_edge_5 = Edge(debug_vertex_6)
+    debug_vertex_5.edges.append(debug_edge_5)
+
+    debug_edge_6 = Edge(debug_vertex_1)
+    debug_vertex_6.edges.append(debug_edge_6)
+
+    self.vertexes.extend([debug_vertex_1, debug_vertex_2, debug_vertex_3, debug_vertex_4, debug_vertex_5, debug_vertex_6])
 
   def bfs(self, start):
     random_color = "#"+''.join([random.choice('0123456789ABCDEF') for j in range(6)])
